b9fd02fb7a607b8b016485342d21c751faeb8457
Göran Persson <guranxp@gmail.com>
2025-10-11 20:35:44 +0200

    Added minimal Java class and JUnit test for v1-plain-java

---
+++ v1-plain-java/src/main/java/com/guranxp/App.java
@@ -0,0 +1,11 @@
+package com.guranxp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from v1-plain-java!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
