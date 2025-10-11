a1efb4b9310e8f86876773f17ed702052bd3bb6c
Göran Persson <guranxp@gmail.com>
2025-10-11 20:39:58 +0200

    Fixed package names.

--- v1-plain-java/src/main/java/com/guranxp/v1/App.java
+++ v1-plain-java/src/main/java/com/guranxp/v1/App.java
@@ -1,4 +1,4 @@
-package com.guranxp;
+package com.guranxp.v1;
 
 public class App {
     public static void main(String[] args) {
--- v1-plain-java/src/test/java/com/guranxp/v1/AppTest.java
+++ v1-plain-java/src/test/java/com/guranxp/v1/AppTest.java
@@ -1,6 +1,7 @@
-package com.guranxp;
+package com.guranxp.v1;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class AppTest {
