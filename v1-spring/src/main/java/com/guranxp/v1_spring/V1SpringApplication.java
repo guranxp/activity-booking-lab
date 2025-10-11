3959d271dfc84e240cd53077cd7488c262cad596
Göran Persson <guranxp@gmail.com>
2025-10-11 21:19:46 +0200

    Added v1-spring module.

---
+++ v1-spring/src/main/java/com/guranxp/v1_spring/V1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.guranxp.v1_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V1SpringApplication.class, args);
+	}
+
+}
---
+++ v1-spring/src/test/java/com/guranxp/v1_spring/V1SpringApplicationTests.java
@@ -0,0 +1,15 @@
+package com.guranxp.v1_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+class V1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
